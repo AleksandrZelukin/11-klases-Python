bcbd4298623af172d4b110d6006db6e3f9b8e08f
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-01-31 10:26:42 +0200

    Update apps

--- 11b_skoleni.py
+++ 11b_skoleni.py
@@ -10,13 +10,29 @@ cur.execute("""CREATE TABLE IF NOT EXISTS  skolens(
     uzvards text,
     epasta text)""")
 
-num = int(input("Skolēna kartējas numurs: "))
-vard = input("Skolēna vārds:")
-uzv = input("Skolēna uzvārds: ")
-pasts = input("Skolēna epasta: ")
-saraksts = (num,vard,uzv,pasts)
+cur.execute("""CREATE TABLE IF NOT EXISTS prieksmeti(
+    ID_prieksmets INT PRIMARY KEY,
+    nosaukums text,
+    skolena_id INT,
+    FOREIGN KEY (skolena_id) REFERENCES skolens (ID_skolens)
+    )""")
 
-cur.execute("""INSERT INTO skolens(ID_skolens,vards,uzvards,epasta)VALUES(?,?,?,?)""",saraksts)
+
+# num = int(input("Skolēna kartējas numurs: "))
+# vard = input("Skolēna vārds:")
+# uzv = input("Skolēna uzvārds: ")
+# pasts = input("Skolēna epasta: ")
+# saraksts = (num,vard,uzv,pasts)
+
+# prieks = int(input("Priekšmeta numurs: "))
+# nos = input("Priekšmeta nosaukums: ")
+# skol = int(input("Skolēna numurs: "))
+# prieksmet = (prieks,nos,skol)
+
+# cur.execute("""INSERT INTO skolens(ID_skolens,vards,uzvards,epasta)VALUES(?,?,?,?)""",saraksts)
+# cur.execute("""INSERT INTO prieksmeti(ID_prieksmets,nosaukums,skolena_ID)VALUES(?,?,?)""",prieksmet)
+
+cur.execute("SELECT * FROM prieksmeti, skolens WHERE skolena_ID = skolens(ID_skolens)")
 
 baza.commit()
 cur.close()
