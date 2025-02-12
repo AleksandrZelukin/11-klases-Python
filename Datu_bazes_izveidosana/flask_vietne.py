43f9e66b8daf54f4d24d4fd697e00150cc5de716
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-02-12 09:12:56 +0200

    Update apps

--- Datu_bazes_izveidosana/flask_vietne.py
+++ Datu_bazes_izveidosana/flask_vietne.py
@@ -1,6 +1,6 @@
-from flask import *
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-app.run(host="0.0.0.0",port="8080")
+app.run(host="0.0.0.0",port="5000")
