c158f8d035d0523fe28d7081533be6187265ebca
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-04-04 10:11:42 +0300

    Update apps

--- vardnica_ievads.py
+++ vardnica_ievads.py
@@ -2,10 +2,22 @@ a = {"Rīga": "Latvija", "Berlīne": "Vācija",
      "Viļņa": "Lietuva", "Tallina": "Igaunija",
      "Helsinki": "Somija", "Oslo": "Norvēģija"}
 
-print(a)
+# print(a)
 
 a["Praha"] = "Čehija"
-print(a)
+# print(a)
 a.pop("Helsinki")
 del a["Oslo"]
-print(a["Rīga"])
+# print(a["Rīga"])
+
+# for i in a:
+#     print(i, a[i])
+
+while True:
+    valsts = input("Ievadi galvaspilsētu: ")
+    if valsts in a:
+        print(valsts,"- galvaspisēta ",a[valsts])
+        break
+    else:
+        print("Valsts nav datu bāzē.")
+        break
