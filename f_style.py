57b070662bbd8a96c28376476f111b904decdc33
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-05-12 12:01:53 +0300

    Create apps

---
+++ f_style.py
@@ -0,0 +1,5 @@
+price = 59
+print(f"The price is {price} dollars")
+print("The price is {} dollars".format(price))
+print("The price is %d dollars" % price)
+print("The price is {0} dollars".format(price))
