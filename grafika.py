a2d3dab917c1267e813ef268b07b538d51dca6ed
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-10-03 09:20:45 +0300

    Update apps

--- grafika.py
+++ grafika.py
@@ -1,16 +1,26 @@
-import turtle
+from turtle import *
+from random import choice
+speed(100)
+x=(0, 200, 400, 600, -200, -400, -600)
+y=(0, 200, 400, 600, -200, -400, -600)
+# Move to starting position
+def start_position():
+    penup()
+    goto(choice(x), choice(y))
+    pendown()
 
-screen = turtle.Screen()
-screen.setup(550, 550)
+def draw_square(size):
+    for _ in range(4):
+        forward(size)
+        right(90)
+        
+def draw_pattern():
+    colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+    start_position()
+    for i in range(36):
 
-t = turtle.Turtle()
-
-for i in range(4):
-    t.forward(100)
-    t.left(90)
-t.right(90)
-for i in range(4):
-    t.forward(100)
-    t.right(90)
-t.home()   
-turtle.mainloop()
+        pencolor(colors[i % len(colors)])
+        draw_square(100)
+        right(10)
+    done()
+draw_pattern()
