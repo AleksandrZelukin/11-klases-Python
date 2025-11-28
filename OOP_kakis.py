6ff61a82c317708e8933cd97f0a19b7d00475e6b
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-11-28 12:27:34 +0200

    Create apps

---
+++ OOP_kakis.py
@@ -0,0 +1,9 @@
+class Kakis:
+    def __init__(self,name,age,poroda):
+        self.name = name
+        self.age = age
+        self.poroda = poroda
+               
+kakis1 = Kakis("Barsik",2,"pers")
+
+print(kakis1.name,kakis1.age,kakis1.poroda)    
